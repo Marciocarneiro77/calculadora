69c342b9bc6e21792183c89d4514115cf2689330
Marcio Carneiro 77 <mac.carneiro@yahoo.com.br>
2020-12-10 17:29:08 -0300

    exercicio calculadora

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,3 +1,7 @@
+function Inserir0(){
+    document.getElementById("visor").value += 0;
+}
+
 function Inserir7(){
     document.getElementById("visor").value += 7;
 }
@@ -10,25 +14,32 @@ function InserirMais(){
     document.getElementById("visor").value += "+";
 }
 
+function InserirMulti(){
+    document.getElementById("visor").value += "*";
+}
+
 function Inserir9(){
     document.getElementById("visor").value += 9;
 }
 
-
-function LimparVisor(){
-    document.getElementById("visor").value = "" ;
+function InserirDiv(){
+    document.getElementById("visor").value += "/";
 }
 
 function Inserir4(){
     document.getElementById("visor").value += 4;
 }
 
+function Inserir5(){
+    document.getElementById("visor").value += 5;
+}
+
 function Inserir6(){
     document.getElementById("visor").value += 6;
 }
 
-function Inserir5(){
-    document.getElementById("visor").value += 5;
+function InserirSub(){
+    document.getElementById("visor").value += "-";
 }
 
 function Inserir1(){
@@ -42,26 +53,36 @@ function Inserir2(){
 function Inserir3(){
     document.getElementById("visor").value += 3;
 }
-function Inserir0(){
-    document.getElementById("visor").value += 0;
+
+function InserirPt(){
+    document.getElementById("visor").value += ".";
+}
+
+function LimparVisor(){
+    document.getElementById("visor").value = "";
 }
 
 function RaizQuadrada(){
     let numero = document.getElementById("visor").value;
     document.getElementById("visor").value = Math.sqrt(numero);
-
 }
 
 function Calcular(){
     //pegar o texto do visor
-    let expressao = document.getElementById("visor.value");
+    let expresao= document.getElementById("visor").value;
 
+
+    //converter em expressão numérica e calcular
+    let resultado = eval(expresao);
+
+    //mostrar o resultado do cálculo no visor no visor
+    document.getElementById("visor").value = resultado;
 }
-//converter em expressão numérica e calcular
-let resultado = eval(expressao);
 
-//mostrar o resultado do calculo no visor 
-document.getElementById("visor".value = resultado);
+function Apagar(){
+let conteudo = document.getElementById("visor").value;
+document.getElementById("visor").value = conteudo.substring(0, conteudo.length - 1);
+}
 
 
 
