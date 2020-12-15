834a6d874ebf728776a94f115d6b0709a8bf6890
Marcio Carneiro 77 <mac.carneiro@yahoo.com.br>
2020-12-15 18:59:18 -0300

    mudanças no exercicio de calculadora

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,62 +1,9 @@
-function Inserir0(){
-    document.getElementById("visor").value += 0;
+function Inserir(elemento){
+    document.getElementById("visor").value += elemento;
 }
 
-function Inserir7(){
-    document.getElementById("visor").value += 7;
-}
 
-function Inserir8(){
-    document.getElementById("visor").value += 8;
-}
 
-function InserirMais(){
-    document.getElementById("visor").value += "+";
-}
-
-function InserirMulti(){
-    document.getElementById("visor").value += "*";
-}
-
-function Inserir9(){
-    document.getElementById("visor").value += 9;
-}
-
-function InserirDiv(){
-    document.getElementById("visor").value += "/";
-}
-
-function Inserir4(){
-    document.getElementById("visor").value += 4;
-}
-
-function Inserir5(){
-    document.getElementById("visor").value += 5;
-}
-
-function Inserir6(){
-    document.getElementById("visor").value += 6;
-}
-
-function InserirSub(){
-    document.getElementById("visor").value += "-";
-}
-
-function Inserir1(){
-    document.getElementById("visor").value += 1;
-}
-
-function Inserir2(){
-    document.getElementById("visor").value += 2;
-}
-
-function Inserir3(){
-    document.getElementById("visor").value += 3;
-}
-
-function InserirPt(){
-    document.getElementById("visor").value += ".";
-}
 
 function LimparVisor(){
     document.getElementById("visor").value = "";
